e4f9334e8d3cb6946efaa04687999ab6b0d504bf
zahid-soft <zahidsarang95@gmail.com>
2021-11-20 17:01:26 +0530

    Code Readability problem solve

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,51 +1,22 @@
-import React,{useState}from 'react'
+import React,{createContext, useState}from 'react'
 import './Cart.css';
-import { Scrollbars } from 'react-custom-scrollbars-2';
-import Items from './Items';
-import {products} from "./Product"
 
+import {products} from "./Product";
+
+import ContextCart from './ContextCart';
+
+
+export const CartContext = createContext();
 
 function Cart() {
 
     const [item,setItem] = useState(products);
 
-
-
-
     return (
         <>
-        <header>
-            <div className="continue-shopping">
-                <img src="./Images/arrow.png" alt="arrow" className="arrow-icon"/>
-                <h3>Continue shopping</h3>
-            </div>
-            <div className="cart-icon">
-                <img src="./Images/cart.png" alt="cart" />
-                <p>7</p>
-            </div>
-        </header>
-
-        <section className="main-cart-section">
-            <h1>Shopping Cart</h1>
-            <p className="total-items">you have <span className="total-items-count">7</span> items in shopping cart</p>
-            <div className="cart-items">
-                   
-                <div className="cart-items-container">
-                <Scrollbars>
-                {
-                    item.map((curItem) => {
-                        return <Items key={curItem.id} {...curItem}/>
-
-                    })
-                }
-                </Scrollbars>
-                </div>
-            </div>
-            <div className="card-total">
-                <h3>Cart Total : <span>22000rs</span></h3>
-                <button>checkout</button>
-            </div>
-        </section>
+        <ContextCart>
+        <ContextCart />
+        </ContextCart>
         </>
     )
 }
